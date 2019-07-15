07e981d6d64687922256db3fa768b59f95d0636a
Igor Antonio <igor.brito95@gmail.com>
2019-07-15 17:43:15 +1000

    Mudancas feitas na quinta

--- index.php
+++ index.php
@@ -1,13 +1,41 @@
+<?php
 
-<h1> <?php print hello($name) ?> </h1>
-<ul>
+$a = 3; //Atribui os valores as variaveis
+$b = 4;
 
-    <?php while (have_posts()): the_post (); ?>
+echo ($a * $b); //Imprime as variaveis B e B
 
-    <li> <?php the_title() ?></li>
-        <?php the_excerpt() ?> 
+$colours = ['red', 'yellow', 'green']; // Array
+echo '<p>' . 'Colour: ' . $colours[0] . '.<p>';  //O zero e onde tudo comeca, logo 0 = red, 1 = yellow, etc.
 
-    <?php endwhile ?>
+function toast($bread) //funcao para imprimir na tela.
+{
+    echo '<p>' . 'Toast ' . $bread . '.<p>';
+}
 
-</ul>
+$KindsOfBread = ['banana bread' , 'rye' , 'white', 'sourdough'];
 
+foreach($KindsOfBread as $bread)
+ {
+ toast($bread);
+ }
+
+function Car($Cars){
+    echo '<p>' . 'Car: ' . $Cars . '.<p>';
+}
+
+
+echo '<ul>';
+
+ $DreamCars = ['WRX STI' , 'RS3' , 'RS4' , 'A45 S' , 'C63 S AMG', '911 GT3 RS', 'Macan GTS', 'Supra' , 'Skyline GT-R R34' , 'GT-R R35' , 'M2 CSL' , 'M4 Competition' , 'Giulia Quadrifoglio' , 'M135i', 'Corvette Stingray' , 'Camaro Z28', 'Sesto Elemento' , 'Intenza Emozione' , 'Civic Type R' , 'Golf R32'];
+
+ foreach($DreamCars as $cars){
+     WrapperContent($cars);
+ }
+
+function WrapperContent($content)
+
+{
+    echo '<li>' . 'Cars: '. $content . '</li>';
+}
+'</ul>';
