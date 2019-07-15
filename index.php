35737c2c982cdad88b7633c638a5e1d54736814d
Igor Antonio <igor.brito95@gmail.com>
2019-07-15 20:56:15 +1000

    Mudanca no layout do site e comentarios no arquivo CSS

--- index.php
+++ index.php
@@ -3,21 +3,23 @@
     <head>
         <title>True Fake News</title>
         <link rel="stylesheet" href="<?php echo get_stylesheet_uri() ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     </head>
 
     <body>
 
         <h1>latest News</h1>
-        <ul class="bar">
+        <ul class="row">
             <?php while(have_posts()) : the_post(); ?>
-            <li class="bar-item">
-             <div class="card">
-            <h2 class="car-title">
-        <?php the_title() ?></li>
-            <h2>
-                <?php the_post_thumbnail('thumbnail'); ?>
+            <li class="column">
+               <div class="card">
+               <h2 class="card-title"><?php the_title() ?></h2>
+                <div class="card-image"> <?php the_post_thumbnail(); ?></div>
+                <div class="card-content"> <?php the_excerpt()?></div>
                 <?php the_content() ?>
-            <?php endwhile ?> 
+                </div>
+            <?php endwhile ?>   
+            </li>
         </ul>
 
     </body>
