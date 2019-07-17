46b4bb050133803b42fbe867ee9f770e02960dcb
Igor Antonio <igor.brito95@gmail.com>
2019-07-17 19:32:45 +1000

    Site aperfeicoado

--- index.php
+++ index.php
@@ -1,23 +1,31 @@
+<html>
 <head>
     <title>True Fake News | This is your favorite and only trustworthy Fake News website!</title>
     <link rel="stylesheet" href="http://truefakenews.local/wp-content/themes/TrueFakeNews_Default/style.css ">
 </head>
 
 <body>
-    <h1>The Latest News</h1>
-    <ul class="naked-list">
-    <?php $x = 1; while($x <= 5) { ?>
-             <li>
-                <article class="card">
-                 <h2 class='grid'>Post Title</h2>
-                    <img src="https://placehold.it/300*200"/>
-                     <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit eum nobis, ullam at ratione sunt impedit quia, ex sapiente accusantium placeat dolor praesentium, voluptatibus odio. Voluptatem veniam at similique porro!
-                     </p>
-                    <a>Read more</a>
-                </article>
-            </li>
-    <?php $x++; } ?>
-    </ul>
+    <h1>Latest News</h1>
+    <ul class="grid">
 
+
+    <?php while(have_posts()) { the_post(); ?>
+
+        <li class="grid-item">
+            <article class="card">
+                <small><?php echo get_the_date();?></small>
+                <h2><?the_title()?></h2>
+                <div><? the_post_thumbnail()?></div>
+                <p>
+                  <?php the_excerpt()?>
+                </p>
+                <a href="<?php the_permalink(); ?>">Read More</a>
+
+            </article>
+        </li>
+
+    <?php } ?>
+
+    </ul>
 </body>
+<html
