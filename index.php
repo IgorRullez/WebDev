03eb16f7398605fc79fad09724b22952f0e9f54c
Igor Antonio <igor.brito95@gmail.com>
2019-07-17 17:59:32 +1000

    Tirei uma linha ruim

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
              <li>
                 <article class="card">
                  <h2 class='grid'>Post Title</h2>
-                    <img src="https://ichef.bbci.co.uk/news/660/cpsprodpb/14CCB/production/_95159158_fakenews.jpg"/>
+                    <img src="https://placehold.it/300*200"/>
                      <p>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit eum nobis, ullam at ratione sunt impedit quia, ex sapiente accusantium placeat dolor praesentium, voluptatibus odio. Voluptatem veniam at similique porro!
                      </p>
